71ccd69a412212d3fb907cb0102f9e4636400e0d
Oleksandr Sliusarenko <aslyusarenko@gmail.coml>
2022-06-27 17:52:33 +0300

    added requirements for deployment

---
+++ tests/test_app.py
@@ -0,0 +1,72 @@
+import json
+
+import pytest
+from src import app
+
+
+app.testing = True
+client = app.test_client()
+
+
+@pytest.fixture(scope='function', params=[
+    '/recipes',
+    '/ingredients',
+    '/feedback',
+    '/users',
+    '/tracker',
+    '/',
+    '/register',
+    '/login'
+])
+def setup_get(request):
+    endpoints = request.param
+    return endpoints
+
+
+@pytest.fixture(scope='function', params=[
+    '/recipes',
+    '/ingredients',
+    '/feedback',
+    '/users',
+    '/tracker',
+    '/',
+    '/register',
+    '/login'
+])
+def setup_post(request):
+    endpoints = request.param
+    return endpoints
+
+
+def test_endpoints_with_get(setup_get):
+    resp = client.post(setup_get)
+    # print(resp.status_code)
+    # print(resp.content_type)
+    # print(resp.json)
+    assert resp.status_code == 200
+    assert resp.status_code == 302
+    assert resp.status_code == 405
+    assert resp.status_code == 401
+
+
+def test_endpoints_with_post(setup_post):
+    resp = client.post(setup_post)
+    # print(resp.status_code)
+    # print(resp.content_type)
+    # print(resp.json)
+    assert resp.status_code == 200
+
+
+def test_register_with_post():
+    resp = client.post('/register', data=json.dumps(dict(name='alexa', email='alex@mail.com', password='12345678')), content_type='application/json')
+    print(resp.status_code)
+    print(resp.content_type)
+    print(resp.json)
+    assert resp.status_code == 201
+
+def test_login_with_post():
+    resp = client.post('/login', data=json.dumps(dict(name='alexa', password='12345678')), content_type='application/json')
+    print(resp.status_code)
+    print(resp.content_type)
+    print(resp.json)
+    assert resp.status_code == 200
